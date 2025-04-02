dbb079d116d8f8d7dd0c46c853ca834ffa679bb4
ArjunB-1808 <b.arjun180804@gmail.com>
2025-04-03 00:15:09 +0530

    words.js

---
+++ words.js
@@ -0,0 +1,6 @@
+const wordsList = [
+    "apple", "banana", "grape", "orange", "mango",
+    "cherry", "strawberry", "pineapple", "peach", "watermelon",
+    "blueberry", "kiwi", "plum", "melon", "apricot",
+    "pear", "blackberry", "coconut", "fig", "pomegranate"
+];
